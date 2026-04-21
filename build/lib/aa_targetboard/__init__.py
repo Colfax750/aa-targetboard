85517bc52eb166cce365b0f645431189e47885a0
Colfax750 <agolfus@gmail.com>
2026-04-21 11:22:30 -0700

    Remove build and dist artifacts from git

--- build/lib/aa_targetboard/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "1.0.0"
-
-default_app_config = "aa_targetboard.apps.AaTargetboardConfig"
--- build/lib/aa_targetboard/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AaTargetboardConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "aa_targetboard"
-    verbose_name = "Target Board"
